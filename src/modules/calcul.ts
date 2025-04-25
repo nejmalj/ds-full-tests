60c648f5588db349d49b36297a7dc94e07cb7820
nejmalj <nejmalejeannic@gmail.com>
2025-04-25 09:21:07 +0200

    Hour function skeleton

---
+++ src/modules/calcul.ts
@@ -0,0 +1,12 @@
+// mettre un console.log à chaque étape pour vérifier que les pouvoirs sont bien appliqués
+export function getJupiterHour (moonHour: number, sunHour: number, earthHour: number) : string {
+    // result initialisé à 0
+
+    // application des pouvoirs de la lune
+    // application des pouvoirs du soleil
+    // application des pouvoirs de la terre
+    //valeur result finale
+
+    // applique une valeur à jupiterHour (string) selon le resultat obtenu
+    return jupiterHour;
+}
